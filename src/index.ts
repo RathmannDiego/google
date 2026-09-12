e5fd5d2ac60b0afea949ccc35f44600e9aef57a9
Retwet2323 <retwet2323@gmail.com>
2026-09-13 00:31:40 +0200

    index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+
+interface Env {}
+
 export default {
-  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    return new Response("¡Hola  desde Cloudflare con TypeScript!", {
-      headers: { "content-type": "text/plain;charset=UTF-8" },
-    });
-  },
-};
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    return new Response("¡Hola desde Cloudflare con TypeScript!", {
+      headers: { "content-type": "text/plain;charset=UTF-8" },
+    });
+  },
+};  
