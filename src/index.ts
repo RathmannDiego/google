4acce01af443b6ba5d5349b5681dc2596beac002
Retwet2323 <retwet2323@gmail.com>
2026-09-13 03:19:31 +0200

    variables

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
-
 interface Env {}
 
 export default {
-  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    return new Response("¡Hola desde Cloudflare con TypeScript! y", {
-      headers: { "content-type": "text/plain;charset=UTF-8" },
-    });
-  },
-};  
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    const branch = ""; 
+    const repo = "";   
+    const worker = ""; 
+    const base = "";   
+
+    const mensaje = `¡Hola desde Cloudflare con TypeScript! y\nRAMA: ${branch}\nRepositorio: ${repo}\nWORKER: ${worker}\nBASE: ${base}`;
+
+    return new Response(mensaje, {
+      headers: { "content-type": "text/plain;charset=UTF-8" },
+    });
+  },
+};
