ca42506a621ecaca1a754186162e54f32e68d4fb
Retwet2323 <retwet2323@gmail.com>
2026-09-13 11:36:52 +0200

    33

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,27 @@
 export interface Env {
   DB: D1Database;
   ENVIRONMENT: string;
-  DB_NAME: string; 
+  DB_NAME: string;
 }
+
+// Variables que actualizará el script de Node.js
+const branch = "desconocida";
+const repo = "google";
+const worker = "desconocido";
+const base = "no-definida";
 
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
     try {
-      // Consulta de prueba a la base de datos del entorno actual
       const { results } = await env.DB.prepare("SELECT 1 as test").all();
       const estadoDb = results ? "Activa y Conectada" : "Sin respuesta";
       
       return new Response(
-        `¡Hola desde Cloudflare con TypeScript   vacio   !\n\n` +
-        `RAMA: (Gestionada en servidor)\n` +
-        `Repositorio: google\n` +
-        `WORKER: ${env.ENVIRONMENT || 'desconocido'}\n` +
-        `BASE: ${env.DB_NAME || 'no-definida'} - [${estadoDb}]`
+        `¡Hola desde Cloudflare con TypeScript!\n\n` +
+        `RAMA: ${branch}\n` +
+        `Repositorio: ${repo}\n` +
+        `WORKER: ${env.ENVIRONMENT || worker}\n` +
+        `BASE: ${env.DB_NAME || base} - [${estadoDb}]`
       );
     } catch (e: any) {
       return new Response(`Error al conectar con la base de datos: ${e.message}`, { status: 500 });
